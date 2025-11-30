3dfbffb4df9688d8ae3447ee0c3cdf958905eb1a
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2025-11-30 15:10:26 +0100

    try_from_into

--- exercises/23_conversions/try_from_into.rs
+++ exercises/23_conversions/try_from_into.rs
@@ -28,14 +28,34 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
 
-    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {}
+    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let range = 0..=255;
+        if !range.contains(&tuple.0) || !range.contains(&tuple.1) || !range.contains(&tuple.2) {
+            return Err(IntoColorError::IntConversion);
+        }
+
+        let red = tuple.0 as u8;
+        let green = tuple.1 as u8;
+        let blue = tuple.2 as u8;
+        Ok(Color { red, green, blue })
+    }
 }
 
 // TODO: Array implementation.
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
 
-    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {}
+    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let range = 0..=255;
+        if !range.contains(&arr[0]) || !range.contains(&arr[1]) || !range.contains(&arr[2]) {
+            return Err(IntoColorError::IntConversion);
+        }
+
+        let red = arr[0] as u8;
+        let green = arr[1] as u8;
+        let blue = arr[2] as u8;
+        Ok(Color { red, green, blue })
+    }
 }
 
 // TODO: Slice implementation.
@@ -43,7 +63,21 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
 
-    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {}
+    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+        let range = 0..=255;
+        if !range.contains(&slice[0]) || !range.contains(&slice[1]) || !range.contains(&slice[2]) {
+            return Err(IntoColorError::IntConversion);
+        }
+
+        let red = slice[0] as u8;
+        let green = slice[1] as u8;
+        let blue = slice[2] as u8;
+
+        Ok(Color { red, green, blue })
+    }
 }
 
 fn main() {
